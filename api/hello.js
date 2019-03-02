a728440daa91ae2202ae3b09d2005731c05bd504
Dustin Whisman <whisman.dustin@gmail.com>
2019-03-02 07:41:44 -0600

    Test destructuring process.env.

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
-exports.handler = async () => ({
-  statusCode: 200,
-  body: `NODE_ENV: ${process.env.NODE_ENV}, SECOND_VAR: ${process.env.SECOND_VAR}`,
-});
+exports.handler = async () => {
+  const { NODE_ENV, SECOND_VAR, THIRD_VAR } = process.env;
+  return {
+    statusCode: 200,
+    body: `NODE_ENV: ${NODE_ENV}, SECOND_VAR: ${SECOND_VAR}, THIRD_VAR: ${THIRD_VAR}`,
+  };
+};
