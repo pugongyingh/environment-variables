ce03627a28d2ad920cdb48d13d58d57eccd9bf85
Dustin Whisman <whisman.dustin@gmail.com>
2019-03-02 07:50:41 -0600

    Test direct access.

--- api/hello.js
+++ api/hello.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 
 exports.handler = async () => {
-  const { NODE_ENV, SECOND_VAR, THIRD_VAR } = process.env;
   return {
     statusCode: 200,
-    body: `NODE_ENV: ${NODE_ENV}, SECOND_VAR: ${SECOND_VAR}, THIRD_VAR: ${THIRD_VAR}`,
+    body: `NODE_ENV: ${process.env.NODE_ENV}, SECOND_VAR: ${process.env.SECOND_VAR}, THIRD_VAR: ${process.env.THIRD_VAR}`,
   };
 };
