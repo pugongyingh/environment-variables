268db21a49a5befa26f5ce0766ed7f1f6573f2d6
Dustin Whisman <whisman.dustin@gmail.com>
2019-03-02 07:30:56 -0600

    Add environment variable.

--- api/hello.js
+++ api/hello.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 exports.handler = async () => ({
   statusCode: 200,
-  body: `NODE_ENV: ${process.env.NODE_ENV}`,
+  body: `NODE_ENV: ${process.env.NODE_ENV}, SECOND_VAR: ${process.env.SECOND_VAR}`,
 });
